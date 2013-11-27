87bdb9fa4fcc20ff027e86db3e33194111e47187
hans <hans@gmail.com>
2013-11-27 23:51:04 +0100

    layout begin

--- gen/com/example/arlam/R.java
+++ gen/com/example/arlam/R.java
@@ -10,6 +10,12 @@ package com.example.arlam;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int color_activate=0x7f040000;
+        public static final int color_black_line=0x7f040002;
+        public static final int color_button=0x7f040003;
+        public static final int color_deactivate=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +23,48 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int button_height=0x7f050007;
+        public static final int button_width=0x7f050008;
+        public static final int header_text_size=0x7f050005;
+        public static final int row_height=0x7f050002;
+        public static final int row_margin=0x7f050004;
+        public static final int row_width=0x7f050003;
+        public static final int small_header_text_size=0x7f050006;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f09000d;
+        public static final int button_alarm_activate=0x7f090009;
+        public static final int button_alarm_deactivate=0x7f09000c;
+        public static final int tableRow1=0x7f090000;
+        public static final int tableRow1_2=0x7f090002;
+        public static final int tableRow2=0x7f090004;
+        public static final int tableRow3=0x7f090006;
+        public static final int tableRow4=0x7f090008;
+        public static final int tableRow5=0x7f09000b;
+        public static final int textView1=0x7f090005;
+        public static final int textView_alarm=0x7f090001;
+        public static final int timePicker1=0x7f090007;
+        public static final int view_activate=0x7f09000a;
+        public static final int view_line=0x7f090003;
     }
     public static final class layout {
         public static final int activity_alarm=0x7f030000;
     }
     public static final class menu {
-        public static final int alarm=0x7f070000;
+        public static final int alarm=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int uhrzeit=0x7f060003;
+        public static final int wecker=0x7f060002;
+        public static final int wecker_aktivieren=0x7f060004;
+        public static final int wecker_deaktivieren=0x7f060005;
     }
     public static final class style {
         /** 
@@ -59,10 +88,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
